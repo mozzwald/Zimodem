481f27ef35d7242d06da3285f61a6f2e4dd352ea
mozzwald <mozzwald@mozzwald.com>
2019-11-20 09:30:27 -0600

    Import FujiNet command parsing. Now it will boot from an ATR in SPIFFS. Added autorun.atr which has RVerter, BobTerm & IceT800

--- zimodem/zimodem.ino
+++ zimodem/zimodem.ino
@@ -75,7 +75,7 @@ const char compile_date[] = __DATE__ " " __TIME__;
 # define DEFAULT_PIN_RI 14
 # define DEFAULT_PIN_RTS 4
 # define DEFAULT_PIN_CTS 5 // is 0 for ESP-01, see getDefaultCtsPin() below.
-# define DEFAULT_PIN_DCD 2
+# define DEFAULT_PIN_DCD 0
 # define DEFAULT_PIN_MTR 16 // Atari Motor Ctrl
 # define DEFAULT_PIN_CMD 12 // Atari Command
 # define DEFAULT_FCT FCT_AMTRCTL // Atari Motor Ctrl
@@ -84,6 +84,37 @@ const char compile_date[] = __DATE__ " " __TIME__;
 # define preEOLN(...)
 # define echoEOLN(...) serial.prints(EOLN)
 #endif
+
+/* START Atari COMMAND */
+#include <FS.h>
+
+// Uncomment for Debug on 2nd UART (GPIO 2)
+#define DEBUG_S
+
+enum {ID, COMMAND, AUX1, AUX2, CHECKSUM, ACK, NAK, PROCESS, WAIT} cmdState;
+#define DELAY_T5          1500
+#define READ_CMD_TIMEOUT  12
+#define CMD_TIMEOUT       50
+#define STATUS_SKIP       8
+unsigned long cmdTimer = 0;
+byte statusSkipCount = 0;
+byte sector[128];
+volatile bool cmdFlag=false;
+File atr;
+
+union
+{
+  struct
+  {
+    unsigned char devic;
+    unsigned char comnd;
+    unsigned char aux1;
+    unsigned char aux2;
+    unsigned char cksum;
+  };
+  byte cmdFrameData[5];
+} cmdFrame;
+/* END Atari COMMAND */
 
 #ifdef RS232_INVERTED
 # define DEFAULT_DCD_HIGH  HIGH
@@ -203,6 +234,314 @@ static int dtrActive = DEFAULT_DTR_HIGH;
 static int dtrInactive = DEFAULT_DTR_LOW;
 static int dsrActive = DEFAULT_DSR_HIGH;
 static int dsrInactive = DEFAULT_DSR_LOW;
+
+/* START Atari COMMAND */
+/**
+   calculate 8-bit checksum.
+*/
+byte sio_checksum(byte* chunk, int length)
+{
+  int chkSum = 0;
+  for (int i = 0; i < length; i++) {
+    chkSum = ((chkSum + chunk[i]) >> 8) + ((chkSum + chunk[i]) & 0xff);
+  }
+  return (byte)chkSum;
+}
+
+/**
+   ISR for falling COMMAND
+*/
+void ICACHE_RAM_ATTR sio_isr_cmd()
+{
+  cmdFlag=true;
+}
+
+/**
+   Get ID
+*/
+void sio_get_id()
+{
+  cmdFrame.devic = Serial.read();
+  if (cmdFrame.devic == 0x31)
+    cmdState = COMMAND;
+  else
+  {
+    cmdState = WAIT;
+    cmdTimer = 0;
+    sio_reset_serial();
+  }
+
+#ifdef DEBUG_S
+  Serial1.print("CMD DEVC: ");
+  Serial1.println(cmdFrame.devic, HEX);
+#endif
+}
+
+/**
+   Get Command
+*/
+
+void sio_get_command()
+{
+  cmdFrame.comnd = Serial.read();
+  cmdState=AUX1;
+
+//  if (cmdFrame.comnd == 'R' || cmdFrame.comnd == 'W' || cmdFrame.comnd == 'P' || cmdFrame.comnd == 'S' )
+//    cmdState = AUX1;
+//  else
+//  {
+//    cmdState = WAIT;
+//    cmdTimer = 0;
+//  }
+
+#ifdef DEBUG_S
+  Serial1.print("CMD CMND: ");
+  Serial1.println(cmdFrame.comnd, HEX);
+#endif
+}
+
+/**
+   Get aux1
+*/
+void sio_get_aux1()
+{
+  cmdFrame.aux1 = Serial.read();
+  cmdState = AUX2;
+
+#ifdef DEBUG_S
+  Serial1.print("CMD AUX1: ");
+  Serial1.println(cmdFrame.aux1, HEX);
+#endif
+}
+
+/**
+   Get aux2
+*/
+void sio_get_aux2()
+{
+  cmdFrame.aux2 = Serial.read();
+  cmdState = CHECKSUM;
+
+#ifdef DEBUG_S
+  Serial1.print("CMD AUX2: ");
+  Serial1.println(cmdFrame.aux2, HEX);
+#endif
+}
+
+/**
+   Get Checksum, and compare
+*/
+void sio_get_checksum()
+{
+  byte ck;
+  cmdFrame.cksum = Serial.read();
+  ck = sio_checksum((byte *)&cmdFrame.cmdFrameData, 4);
+
+#ifdef DEBUG_S
+    Serial1.print("CMD CKSM: ");
+    Serial1.print(cmdFrame.cksum, HEX);
+#endif
+
+    if (ck == cmdFrame.cksum)
+    {
+#ifdef DEBUG_S
+      Serial1.println(", ACK");
+#endif
+      sio_ack();
+    }
+    else
+    {
+#ifdef DEBUG_S
+      Serial1.println(", NAK");
+#endif
+      sio_nak();
+    }
+}
+
+/**
+   Process command
+*/
+
+void sio_process()
+{
+  switch (cmdFrame.comnd)
+  {
+    case 'R':
+      sio_read();
+      break;
+    case 'S':
+      sio_status();
+      break;
+    case '!':
+      sio_format();
+      break;
+  }
+
+  sio_reset_serial();
+  cmdState = WAIT;
+  cmdTimer = 0;
+}
+
+/**
+   format (fake)
+*/
+void sio_format()
+{
+  byte ck;
+
+  for (int i=0;i<128;i++)
+    sector[i]=0;
+
+  sector[0]=0xFF; // no bad sectors.
+  sector[1]=0xFF;
+
+  ck = sio_checksum((byte *)&sector, 128);
+
+  delayMicroseconds(DELAY_T5); // t5 delay
+  Serial.write('C'); // Completed command
+  Serial.flush();
+
+  // Write data frame
+  Serial.write(sector,128);
+
+  // Write data frame checksum
+  Serial.write(ck);
+  Serial.flush();
+  delayMicroseconds(200);
+#ifdef DEBUG_S
+  Serial1.printf("We faked a format.\n");
+#endif
+}
+
+/**
+   Read
+*/
+void sio_read()
+{
+  byte ck;
+  byte sector[128];
+  int offset =(256 * cmdFrame.aux2)+cmdFrame.aux1;
+  offset *= 128;
+  offset -= 128;
+  offset += 16; // skip 16 byte ATR Header
+  atr.seek(offset, SeekSet);
+  atr.read(sector, 128);
+
+  ck = sio_checksum((byte *)&sector, 128);
+
+  delayMicroseconds(1500); // t5 delay
+  Serial.write('C'); // Completed command
+  Serial.flush();
+
+  // Write data frame
+  Serial.write(sector,128);
+
+  // Write data frame checksum
+  Serial.write(ck);
+  Serial.flush();
+  delayMicroseconds(200);
+#ifdef DEBUG_S
+  Serial1.print("SIO READ OFFSET: ");
+  Serial1.print(offset);
+  Serial1.print(" - ");
+  Serial1.println((offset + 128));
+#endif
+}
+
+/**
+   Status
+*/
+void sio_status()
+{
+  byte status[4] = {0x00, 0xFF, 0xFE, 0x00};
+  byte ck;
+
+  ck = sio_checksum((byte *)&status, 4);
+
+  delayMicroseconds(DELAY_T5); // t5 delay
+  Serial.write('C'); // Command always completes.
+  Serial.flush();
+  delayMicroseconds(200);
+  //delay(1);
+
+  // Write data frame
+  for (int i = 0; i < 4; i++)
+    Serial.write(status[i]);
+
+  // Write checksum
+  Serial.write(ck);
+  Serial.flush();
+  delayMicroseconds(200);
+}
+
+/**
+   Send an acknowledgement
+*/
+void sio_ack()
+{
+  delayMicroseconds(500);
+  Serial.write('A');
+  Serial.flush();
+  //cmdState = PROCESS;
+  sio_process();
+}
+
+/**
+   Send a non-acknowledgement
+*/
+void sio_nak()
+{
+  delayMicroseconds(500);
+  Serial.write('N');
+  Serial.flush();
+  cmdState = WAIT;
+  cmdTimer = 0;
+}
+
+void sio_incoming(){
+  switch (cmdState)
+  {
+    case ID:
+      sio_get_id();
+      break;
+    case COMMAND:
+      sio_get_command();
+      break;
+    case AUX1:
+      sio_get_aux1();
+      break;
+    case AUX2:
+      sio_get_aux2();
+      break;
+    case CHECKSUM:
+      sio_get_checksum();
+      break;
+    case ACK:
+      sio_ack();
+      break;
+    case NAK:
+      sio_nak();
+      break;
+    case PROCESS:
+      sio_process();
+      break;
+    case WAIT:
+      //Serial.read(); // Toss it for now
+      cmdTimer = 0;
+      break;
+  }
+}
+
+void sio_reset_serial()
+{
+  HWSerial.begin(baudRate, serialConfig);
+  HWSerial.swap();
+#ifdef DEBUG_S
+  Serial1.println("ZIModem Baud");
+#endif
+}
+
+/* END Atari COMMAND */
 
 static int getDefaultCtsPin()
 {
@@ -373,7 +712,7 @@ void setup()
       pinSupport[i]=true;
     pinSupport[11]=false;
   }
-#endif    
+#endif
   initSDShell();
   currMode = &commandMode;
   if(!SPIFFS.begin())
@@ -382,6 +721,15 @@ void setup()
     SPIFFS.begin();
     debugPrintf("SPIFFS Formatted.");
   }
+#ifdef DEBUG_S
+  Serial1.begin(19200);
+  Serial1.println();
+  Serial1.println("FujiNet ZIModem Started");
+#endif
+  atr=SPIFFS.open("/autorun.atr","r");
+  attachInterrupt(digitalPinToInterrupt(DEFAULT_PIN_CMD), sio_isr_cmd, FALLING);
+  cmdState = WAIT; // Start in SIO Command WAIT State
+  cmdFlag = false;
   HWSerial.begin(DEFAULT_BAUD_RATE, DEFAULT_SERIAL_CONFIG);  //Start Serial
   HWSerial.swap(); //Swap UART Pins for Atari Motor Control (GPIO13-RX & GPIO15-TX)
   commandMode.loadConfig();
@@ -440,10 +788,41 @@ void checkFactoryReset()
 void loop() 
 {
   checkFactoryReset();
-  if(HWSerial.available())
+  if (cmdFlag)
   {
-    currMode->serialIncoming();
+    if (digitalRead(DEFAULT_PIN_CMD) == LOW)
+    {
+      cmdState = ID;
+      cmdTimer = millis();
+      cmdFlag=false;
+      //Change baud rate for SIO and swap the pins
+#ifdef DEBUG_S
+  Serial1.println("SIO Baud");
+#endif
+      HWSerial.begin(19200);
+      HWSerial.swap();
+    }
+  }
+
+  if(HWSerial.available() > 0)
+  {
+    if (cmdState == WAIT)
+    {
+      currMode->serialIncoming();
+    }
+    else
+    {
+      sio_incoming();
+    }
   }
   currMode->loop();
   zclock.tick();
+  if (millis() - cmdTimer > CMD_TIMEOUT && cmdState != WAIT)
+  {
+    Serial1.print("SIO CMD TIMEOUT: ");
+    Serial1.println(cmdState);
+    sio_reset_serial();
+    cmdState = WAIT;
+    cmdTimer = 0;
+  }
 }
